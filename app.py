cb0fcc08f0357ead90201f2bb77306b08ca8ecca
Yiğit Can Başalma <yigit.basalma@gmail.com>
2021-10-10 13:48:11 +0300

    Redis host from os environ

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host=f"{os.environ['REDIS_HOST']}", db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
